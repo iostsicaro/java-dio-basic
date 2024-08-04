3a61de0121df27c44514635f38c1c9def501f516
Icaro <iostsicaro@gmail.com>
2024-08-04 20:07:38 -0300

    Testing the use of scanner.

---
+++ src/src/using_scanner/AboutMe.java
@@ -0,0 +1,24 @@
+package using_scanner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Bem-vindo " + nome + " " + sobrenome);
+    }
+}
